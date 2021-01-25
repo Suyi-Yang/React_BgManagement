5cea3c77da0a7922c73fb70c65dc639a12daec36
lunkhead <ysy_lunk@qq.com>
2021-01-25 20:12:34 +0800

    commit 210125 1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+/* 应用的根组件 */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { Button,message } from "antd";
+
+export default class App extends Component{
+  handleClick = ()=>{
+    message.success('已点击');
+  }
+  render(){
+    return <Button type='primary' onClick={this.handleClick}>按钮</Button>
+  }
 }
-
-export default App;
