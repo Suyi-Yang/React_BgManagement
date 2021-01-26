ae096fb5a58fc4228540ee8cc880f60dc695d2fa
lunkhead <ysy_lunk@qq.com>
2021-01-26 15:23:30 +0800

    commit 210126 loginStatic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 /* 应用的根组件 */
-
 import React, { Component } from "react";
-import { Button,message } from "antd";
+import { BrowserRouter,Route,Switch } from "react-router-dom";
 
-export default class App extends Component{
-  handleClick = ()=>{
-    message.success('已点击');
-  }
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+
+export default class App extends Component{  
   render(){
-    return <Button type='primary' onClick={this.handleClick}>按钮</Button>
+    return (
+      <BrowserRouter>
+        <Switch> {/* 只匹配其中一个 */}
+          <Route path='/login' component={Login}></Route>
+          <Route path='/' component={Admin}></Route>
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,56 @@
+/* 用户登录的路由组件 */
+import React, { Component } from 'react'
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+import './login.less' /* 引入login.less */
+import logo from './images/logo.png' /* 引入logo图片 */
+
+export default class Login extends Component {
+  validatePwd = (rule,value)=>{
+    if (!value) {
+      return Promise.reject('密码必须输入');
+    }else if(value.length < 4){
+      return Promise.reject('密码长度不能小于4位');
+    }else if(value.length > 12){
+      return Promise.reject('密码长度不能大于12位');
+    }else if(!/^[a-zA-Z0-9_]+$/.test(value)){
+      return Promise.reject('密码必须是英文、数字或下划线组成');
+    }else{
+      return Promise.resolve(); // 验证通过
+    }
+  }
+  onFinish = (values) => {    
+    console.log('接收到的表单数据: ', values);    
+  };
+  render() {
+    return (
+      <div className='login'>
+        <header className='login-header'>
+          <img src={logo} alt="logo" />
+          <h1>React项目：后台管理系统</h1>
+        </header>
+        <section className='login-content'>
+          <h2>用户登录</h2>
+          <Form name="normal_login" className="login-form" onFinish={this.onFinish}>
+            <Form.Item name="username" rules={[ /* 声明式验证:直接使用别人定义好的验证规则进行验证 */
+                {required: true, message: '用户名必须输入'},
+                {min: 4, message: '用户名至少4位'},
+                {max: 12, message: '用户名最多12位'},
+                {pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成'}
+                /* 正则：[a-zA-Z0-9_]:匹配一个字符；[...]+:匹配多个字符 */
+              ]} initialValue='admin' /* initialValue指定初始值 */ >
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+            </Form.Item>
+            <Form.Item name="password" rules={[{validator: this.validatePwd}]}>             
+              <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="密码" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
+            </Form.Item>
+          </Form>          
+        </section>
+      </div>
+    )
+  }
+}
